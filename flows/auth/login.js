da0c16897c34daa2538585df63dee2807d685f83
hieuleminh03 <hieulm.data@gmail.com>
2025-04-16 10:32:35 +0700

    add api call for course and term

--- flows/auth/login.js
+++ flows/auth/login.js
@@ -1,11 +1,11 @@
-import { login } from '../../lib/api.js';
+import api from '../../lib/api.js';
 
 export default async () => {
     const email = "admin@example.com";
     const password = "admin123";
 
     try {
-        const data = await login(email, password);
+        const data = await api.auth.login(email, password);
         console.log("Token:", data.token);
     } catch (error) {
         throw error;
